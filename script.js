bc1c96bb99e076c3a256a1fce85d2df5d22be99c
TheGarbage <davide@bucchieri.it>
2021-04-12 17:06:08 +0200

    FINALE
    unificazione file, ottimizzazioni varie, powerPoint, bug input e altro

--- script.js
+++ script.js
@@ -1,3 +1,67 @@
+//FUNZIONI D'APPOGGIO -------------------------------------------------------------------------------------------------------------------------------------------------------
+
+function nascondiGiochi(lettera){
+    for(item of giochiAperti){
+        if(!item.gioco.classList.contains('hidden'))
+            if(item.lista.indexOf(lettera) === -1)
+                item.gioco.classList.add('hidden');
+            else
+                item.lista.splice(item.lista.indexOf(lettera), 1);
+    }
+}
+
+function mostraGiochi(stringa, letteraCancellata){
+    let mostra = true;
+    for(item of giochiAperti){
+        mostra = true;
+        if(item.gioco.classList.contains('hidden')){
+            item.lista = [];
+            for(lettera of item.gioco.querySelector('h5').textContent.toUpperCase())
+                item.lista.unshift(lettera);
+            for(lettera of stringa)
+                if(item.lista.indexOf(lettera) === -1)
+                    mostra = false;
+                else
+                    item.lista.splice(item.lista.indexOf(lettera), 1)
+            if(mostra)
+                item.gioco.classList.remove('hidden');
+        }
+        else 
+            item.lista.unshift(letteraCancellata);
+    }
+}
+
+function chiudiMaggioriDettagli(cliccaQui, gioco){
+    gioco.querySelector('.noClick').classList.remove('noClickSelezionato');
+    gioco.querySelector('.click').classList.add('hidden');
+    gioco.classList.add('giocoMinoriDettagli');
+    gioco.classList.remove('giocoMaggioriDettagli');
+    cliccaQui.textContent = "Clicca per maggiori info";
+    cliccaQui.removeEventListener('click', nascondiDettagli);
+    cliccaQui.addEventListener('click', mostraDettagli);
+}
+
+function creaPreferito(blocco, codice){
+    const giocoPreferito = document.createElement("section");
+    giocoPreferito.classList.add('giocoPreferito');
+    giocoPreferito.dataset.codice = codice + "preferito";
+    const imgGioco = document.createElement("img");
+    imgGioco.src = blocco.querySelector('[data-img-gioco = true]').src;
+    giocoPreferito.appendChild(imgGioco);
+    const div = document.createElement('div');
+    giocoPreferito.appendChild(div);
+    const h5 = document.createElement('h5');
+    h5.textContent = blocco.querySelector("h5").textContent;
+    div.appendChild(h5);
+    const imgPreferiti = document.createElement('img');
+    imgPreferiti.classList.add("pointer");
+    imgPreferiti.dataset.codice = codice;
+    imgPreferiti.src = "Imm-Preferito.jpg";
+    imgPreferiti.addEventListener("click", togliPreferiti);
+    div.appendChild(imgPreferiti);
+    return giocoPreferito;
+}
+
 function creaSottoSito(blocco){
     const sottoSiti = document.querySelector(".sotto-siti");
     const newSottoSito = document.createElement('article');
@@ -11,6 +75,7 @@ function creaSottoSito(blocco){
     imgTornaIndietro.classList.add('pointer');
     imgTornaIndietro.classList.add('tornaIndietro');
     imgTornaIndietro.src = "Imm-TornaIndietro.jpg";
+    imgTornaIndietro.addEventListener('click', chiudiSottoSito);
     header.appendChild(imgTornaIndietro);
     const titoloHeader = document.createElement('h4');
     titoloHeader.textContent = blocco.querySelector('h4').textContent;
@@ -39,7 +104,7 @@ function creaSottoSito(blocco){
     const nonPreferiti = document.createElement('section');
     nonPreferiti.classList.add('nonPreferiti');
     newSottoSito.appendChild(nonPreferiti);
-    const subNonPreferiti = document.createElement('p');
+    const subNonPreferiti = document.createElement('div');
     subNonPreferiti.classList.add('sub-nonPreferiti');
     nonPreferiti.appendChild(subNonPreferiti);
     const textNonPreferiti = document.createElement('li');
@@ -51,6 +116,7 @@ function creaSottoSito(blocco){
     form.textContent = "Cerca:";
     const inputCerca = document.createElement('input'); 
     inputCerca.type = "text";
+    inputCerca.addEventListener('keyup', ricerca);
     form.appendChild(inputCerca); 
     subNonPreferiti.appendChild(form);
     const divNonPreferiti = document.createElement('div');
@@ -87,7 +153,7 @@ function creaSottoSito(blocco){
             imgGioco.dataset.imgGioco = true;
             noClick.appendChild(imgGioco);
             const cliccaQui = document.createElement('p');
-            cliccaQui.textContent = "Clicca per maggiori info";
+            cliccaQui.textContent = "Clicca per più info";
             cliccaQui.classList.add("pointer");
             cliccaQui.addEventListener('click', mostraDettagli);
             noClick.appendChild(cliccaQui);
@@ -100,7 +166,7 @@ function creaSottoSito(blocco){
             click.appendChild(descrizione);
             if(giochiPreferiti.indexOf( imgPreferiti.dataset.codice) !== -1){
                 imgPreferiti.classList.add('hidden');
-                gioco.classList.add('selezionato');
+                gioco.classList.add('selezionatoPreferito');
                 stella.classList.remove('hidden');
                 if(preferiti.classList.contains('hidden'))
                     preferiti.classList.remove('hidden');
@@ -113,6 +179,96 @@ function creaSottoSito(blocco){
     return newSottoSito;
 }
 
+//FUNZIONI LISTENER  -------------------------------------------------------------------------------------------------------------------------------------------------------
+
+function inserisciPreferiti(event){  
+    const codice = event.currentTarget.dataset.codice + "nonPreferito";
+    giochiPreferiti.unshift(event.currentTarget.dataset.codice);
+    const giochi = document.querySelectorAll('[data-codice]');
+    let imgPreferiti;
+    let preferiti;
+    for(item of giochi){
+        if(item.dataset.codice === codice){
+            imgPreferiti = item.querySelector('.stellaPiu');
+            imgPreferiti.classList.add('hidden');
+            item.classList.add('selezionatoPreferito');
+            item.querySelector('.stella').classList.remove('hidden');
+            preferiti = item.parentNode.parentNode.parentNode.querySelector(".preferiti");
+            if(preferiti.classList.contains('hidden'))
+                preferiti.classList.remove('hidden');
+            preferiti.querySelector('.giochi').appendChild(creaPreferito(item, imgPreferiti.dataset.codice));
+        }
+    }
+}
+
+function togliPreferiti(event){
+    const imgPreferiti = event.currentTarget;
+    const codicePreferito =  imgPreferiti.dataset.codice + "preferito";
+    const codiceNonPreferito = imgPreferiti.dataset.codice + "nonPreferito";
+    const giochi = document.querySelectorAll('[data-codice]');
+    let preferiti;
+    let stellaPiu;
+    for(item of giochi){
+        if(item.dataset.codice === codicePreferito){
+            preferiti = item.parentNode.parentNode;
+            item.remove();
+            if(preferiti.querySelector(".giocoPreferito") === null)
+                preferiti.classList.add('hidden');
+        }
+        else if(item.dataset.codice === codiceNonPreferito){
+            stellaPiu = item.querySelector('.stellaPiu');
+            stellaPiu.classList.remove('hidden');
+            item.classList.remove('selezionatoPreferito');
+            item.querySelector('.stella').classList.add('hidden');
+        }
+    }
+    giochiPreferiti.splice(giochiPreferiti.indexOf(imgPreferiti.dataset.codice), 1);
+}
+
+function ricerca(event){
+    const nuovaValue = event.currentTarget.value;
+    let lettera = null;
+    if(vecchiaValue.length < nuovaValue.length){
+        for(let i = 0; i < vecchiaValue.length; i++)
+            if(nuovaValue[i] !== vecchiaValue[i]){
+                lettera = nuovaValue[i];
+                break;
+            }
+        if(lettera === null)
+            lettera =  nuovaValue[nuovaValue.length-1];
+        nascondiGiochi(lettera.toUpperCase());
+    }
+    else if(vecchiaValue.length > nuovaValue.length){
+        for(let i = 0; i < nuovaValue.length; i++)
+            if(vecchiaValue[i] !== nuovaValue[i]){
+                lettera = vecchiaValue[i];
+                break;
+            }
+        if(lettera === null)
+            lettera = vecchiaValue[vecchiaValue.length-1];
+        mostraGiochi(nuovaValue.toUpperCase(), lettera);
+    }
+    vecchiaValue = nuovaValue;
+}
+
+function mostraDettagli(event){
+    const cliccaQui =  event.currentTarget;
+    const blocco = cliccaQui.parentNode.parentNode;
+    blocco.classList.add('giocoMaggioriDettagli');
+    blocco.classList.remove('giocoMinoriDettagli');
+    blocco.querySelector('.click').classList.remove('hidden');
+    blocco.querySelector('.noClick').classList.add('noClickSelezionato');
+    cliccaQui.textContent = "Clicca per minori info";
+    cliccaQui.removeEventListener('click', mostraDettagli);
+    cliccaQui.addEventListener('click', nascondiDettagli);
+}
+
+function nascondiDettagli(event){
+    const cliccaQui =  event.currentTarget;    
+    const gioco = cliccaQui.parentNode.parentNode;
+    chiudiMaggioriDettagli(cliccaQui, gioco);
+}
+
 function apriSottoSito(event){
     const blocco = event.currentTarget;
     let sottoSito = document.querySelector(".sotto-siti [data-tema=" + blocco.dataset.tema + ']');
@@ -120,9 +276,7 @@ function apriSottoSito(event){
         sottoSito.classList.remove('hidden');
     else 
         sottoSito = creaSottoSito(blocco);
-    sottoSito.querySelector('input').addEventListener('keyup', ricerca);
-    sottoSitoAperto = sottoSito;
-    const giochiSitoAperto = sottoSitoAperto.querySelectorAll('.gioco');
+    const giochiSitoAperto = sottoSito.querySelectorAll('.gioco');
     for(item of giochiSitoAperto){
         const giocoAperto= {};
         giocoAperto.gioco = item;
@@ -134,26 +288,73 @@ function apriSottoSito(event){
     const elementiSito = document.querySelectorAll('.sito-principale');
     for(item of elementiSito)
         item.classList.add('opacity');
-    const bodyOverlay =  document.querySelector('#body-overlay');
+    const bodyOverlay = document.querySelector('#body-overlay');
     bodyOverlay.classList.remove('hidden');
-    sottoSito.querySelector("img").addEventListener('click', chiudiSottoSito);
 }
 
-function chiudiSottoSito(){
-    const sottoSiti = document.querySelectorAll(".sotto-sito");
-    sottoSitoAperto.classList.add('hidden');
-    sottoSitoAperto.querySelector("img").addEventListener('click', chiudiSottoSito);
-    sottoSitoAperto.querySelector('input').value = '';
+function chiudiSottoSito(event){
+    const tornaIndietro = event.currentTarget;
+    const sottoSito = tornaIndietro.parentNode.parentNode;
+    sottoSito.classList.add('hidden');
+    sottoSito.querySelector('input').value = '';
     vecchiaValue = '';
-    for(item of giochiAperti)
+    for(item of giochiAperti){
         if(item.gioco.classList.contains('hidden'))
             item.gioco.classList.remove('hidden');
-    sottoSitoAperto.querySelector('input').removeEventListener('keyup', ricerca);
+        if(item.gioco.classList.contains('giocoMaggioriDettagli'))
+            chiudiMaggioriDettagli(item.gioco.querySelector('p'), item.gioco); 
+    }
     giochiAperti = [];
-    sottoSitoAperto = null;
     const elementiSito =  document.querySelectorAll('.sito-principale');
     for(item of elementiSito)
         item.classList.remove('opacity');
     const bodyOverlay =  document.querySelector('#body-overlay');
     bodyOverlay.classList.add('hidden');
+}
+
+function vediDidascalia(event){
+    const blocco = event.currentTarget;
+    const attr = blocco.dataset.tema;
+    blocco.removeEventListener('mouseover', vediDidascalia);
+    for(let id in index){
+        if(attr === index[id])
+            blocco.dataset.tema = id;
+    }
+    blocco.classList.add('falsoHover');
+    const didascalia = blocco.querySelector('p');
+    didascalia.classList.remove('hidden');
+    const overlay = blocco.querySelector('.overlay');
+    overlay.classList.remove('dark-overlay');
+    blocco.addEventListener('mouseout', nascondiDidascalia);
+}
+
+function nascondiDidascalia(event){
+    const blocco = event.currentTarget;
+    blocco.removeEventListener('mouseout', nascondiDidascalia);
+    blocco.classList.remove('falsoHover');
+    const didascalia = blocco.querySelector('p');
+    didascalia.classList.add('hidden');
+    const overlay = blocco.querySelector('.overlay');
+    overlay.classList.add('dark-overlay');
+    blocco.dataset.tema = index[blocco.dataset.tema];
+    blocco.addEventListener('mouseover', vediDidascalia);
+}
+
+//Configurazione Iniziale -------------------------------------------------------------------------------------------------------------------------------------------------------
+
+const index = {
+    primo : 'Fps',
+    secondo: 'Arcade',
+    terzo: 'Quiz',
+    quarto: 'Corsa',
+    quinto: 'VediTutto'
+}
+
+let giochiAperti = [];
+let vecchiaValue = '';
+const giochiPreferiti = [];
+const blocchi = document.querySelectorAll('#blocchi .blocco');
+for (const blocco of blocchi){
+    blocco.addEventListener('mouseover', vediDidascalia);
+    blocco.addEventListener('click', apriSottoSito);
 }
